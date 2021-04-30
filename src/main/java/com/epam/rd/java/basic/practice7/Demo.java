0e3db26039f3a1150b72ba47a9326df2b82891cd
Dimon <beregd2013@gmail.com>
2021-04-30 11:59:24 +0300

    1

--- src/main/java/com/epam/rd/java/basic/practice7/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Demo.java
@@ -1,8 +1,13 @@
 package com.epam.rd.java.basic.practice7;
 
-public final class Demo {
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
-    public static void main(String[] args) throws Exception {
+public final class Demo {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerException, XMLStreamException {
         Main.main(new String[] { "input.xml" });
     }
 }
